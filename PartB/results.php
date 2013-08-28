eb8eae1fd8248e613141b61ee172864e11795c1f
Ubuntu <ubuntu@ip-172-31-24-154.ap-southeast-2.compute.internal>
2013-08-28 07:47:59 +0000

    Added CSS to results.php

--- PartB/results.php
+++ PartB/results.php
@@ -2,6 +2,25 @@
 <html>
 <head>
 <title>Search Results</title>
-This is the search page
+	<style type="text/css">
+		body{
+			font-family: Arial, Verdana, Sans-Serif;
+			font-size: 13px;
+		}
+		h1{
+			font-style: italic;
+			text-align: center;
+		}
+		div#wrapper{
+			margin:auto;
+			width: 1000px;
+		}
+		div#results{
+			padding: 5px;
+			width: 1000px;
+		}
+	</style>
+</head>
+<body>
 </body>
 </html>
