4f747d97bf981cd2cea045765219a2e3ae6b1831
Ubuntu <ubuntu@ip-172-31-24-154.ap-southeast-2.compute.internal>
2013-08-28 08:01:19 +0000

    Added php codings to results.php

--- PartB/results.php
+++ PartB/results.php
@@ -26,6 +26,19 @@
 <h1>Search Results</h1>
 <a href = "inputForm.php">Search Again</a>
 	<div id="results">
+<?php
+		//Setting GET values
+		//Trimmed Whitespace from text input: Reduces Error
+		$wine_name = trim($_GET['wine_name']);
+		$winery_name = trim($_GET['winery_name']);
+		$region_name = $_GET['region_name'];
+		$grape_variety = $_GET['grape_variety'];
+		$min_year = $_GET['min_year'];
+		$max_year = $_GET['max_year'];
+		$min_stock = trim($_GET['min_stock']);
+		$min_ordered = trim($_GET['min_ordered']);
+		$min_price = trim($_GET['min_price']);
+		$max_price = trim($_GET['max_price']);
 </div>
 </body>
 </html>
