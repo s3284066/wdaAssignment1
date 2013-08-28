f06270f5f68487adbd77d49db07dbf8070a9f43c
Ubuntu <ubuntu@ip-172-31-24-154.ap-southeast-2.compute.internal>
2013-08-28 07:16:19 +0000

    Added inputForm.php file

---
+++ PartB/inputForm.php
@@ -0,0 +1,19 @@
+!DOCTYPE html> <html> <head>
+	<title>Input Wine Form</title>
+	<style type="text/css">
+		body{
+			font-family: Arial, Verdana, Sans-Serif;
+			font-size: 13px;
+		}
+		h1{
+			font-style: italic;
+			margin-left: 270px;
+		}
+		div#wrapper{
+			margin:auto;
+			width: 500px;
+		}
+	</style> </head> <body>
+	<div id = wrapper>
+	<h1>Search Winestore:</h1>
+</div> </body> </html>
